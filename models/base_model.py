29c461e29d5a91f29bacfe070649cd3bbc791585
nisselhbb <nissrineelhobbi@gmail.com>
2023-11-07 07:23:58 -0800

    a BaseModel class, that defines common attributes and methods for other classes. We added the to_dict() method to the BaseModel class, enabling the generation of a dictionary representation of an instance. This dictionary rep allows us to recreate a BaseModel instance from it.  Directly saving the dictionary representation to a file is not a suitable option due to several limitations. So, we converted the dictionary rep to a JSON string, which serves as a standard, human-readable data structure representation

---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" import necessary modules """
+
+from uuid import uuid4
+from datetime import datetime
+import models
+""" BaseModelModel calss """
+
+
+class BaseModel:
+    """ BaseModel class """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance.
+            Args:
+                id (str(uuid)): id of the new instance
+        """
+
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+
+    def __str__(self):
+        """ returns [<class name>] (<self.id>) <self.__dict__> """
+        ClassName = self.__class__.__name__
+        return f"[{ClassName}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """ updates the public instance attribute updated_at
+            with the current datetime """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        Dict = {'__class__': self.__class__.__name__}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                value = value.isoformat()
+            Dict[key] = value
+        return Dict
