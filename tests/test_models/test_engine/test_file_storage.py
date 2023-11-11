8e82334be5b1c442c32bc4b748558d66432f0bfd
aboubakr <c.aboubakr@gmail.com>
2023-11-11 14:37:24 +0100

    add test for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""testing the city class"""
+"""testing the FileStorage class"""
 
 import unittest
-from models.city import City
+from models.engine.file_storage import FileStorage
 from datetime import datetime
 
 
-class TestingCity(unittest.TestCase):
+class TestingFileStorage(unittest.TestCase):
     """
-    testing the class city if it exists
+    testing the class FileStorage if it exists
     with its attributes and checking its type
     """
 
-    def test_city(self):
-        thecity = City()
-        self.assertTrue(hasattr(thecity, "name"))
-        self.assertTrue(hasattr(thecity, "id"))
-        self.assertTrue(hasattr(thecity, "state_id"))
-        self.assertTrue(hasattr(thecity, "created_at"))
-        self.assertTrue(hasattr(thecity, "updated_at"))
-
-        self.assertIsInstance(thecity.name, str)
-        self.assertIsInstance(thecity.id, str)
-        self.assertIsInstance(thecity.state_id, str)
-        self.assertIsInstance(thecity.created_at, datetime)
-        self.assertIsInstance(thecity.updated_at, datetime)
+    def test_FileStorage(self):
+        theFileStorage = FileStorage()
+        self.assertTrue(hasattr(theFileStorage, "all"))
+        self.assertTrue(hasattr(theFileStorage, "new"))
+        self.assertTrue(hasattr(theFileStorage, "save"))
+        self.assertTrue(hasattr(theFileStorage, "reload"))
 
 
 if __name__ == "__main__":
