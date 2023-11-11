26a8cc0672568532acb31dd67cb948aae3a23c71
aboubakr <c.aboubakr@gmail.com>
2023-11-11 15:18:47 +0100

    add test for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,8 +2,16 @@
 """testing the FileStorage class"""
 
 import unittest
+import models
 from models.engine.file_storage import FileStorage
 from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class TestingFileStorage(unittest.TestCase):
@@ -25,6 +33,61 @@ class TestingFileStorage(unittest.TestCase):
         
         self.assertIsInstance(file_path, str)
         self.assertTrue(hasattr(fs, '_FileStorage__file_path'))
+
+    def test_all(self):
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_save(self):
+        obj_base = BaseModel()
+        obj_User = User()
+        obj_city = City()
+        obj_state = State()
+        obj_review = Review()
+        obj_place = Place()
+        obj_amenity = Amenity()
+        models.storage.new(obj_base)
+        models.storage.new(obj_User)
+        models.storage.new(obj_state)
+        models.storage.new(obj_place)
+        models.storage.new(obj_amenity)
+        models.storage.new(obj_review)
+        models.storage.new(obj_city)
+        models.storage.save()
+        with open("file.json", "r") as file:
+            data = file.read()
+        self.assertIn(f"BaseModel.{obj_base.id}", data)
+        self.assertIn(f"User.{obj_User.id}", data)
+        self.assertIn(f"State.{obj_state.id}", data)
+        self.assertIn(f"Amenity.{obj_amenity.id}", data)
+        self.assertIn(f"City.{obj_city.id}", data)
+        self.assertIn(f"Review.{obj_review.id}", data)
+        self.assertIn(f"Place.{obj_place.id}", data)
+    def test_reload(self):
+        obj_base = BaseModel()
+        obj_User = User()
+        obj_city = City()
+        obj_state = State()
+        obj_review = Review()
+        obj_place = Place()
+        obj_amenity = Amenity()
+        models.storage.new(obj_base)
+        models.storage.new(obj_User)
+        models.storage.new(obj_state)
+        models.storage.new(obj_place)
+        models.storage.new(obj_amenity)
+        models.storage.new(obj_review)
+        models.storage.new(obj_city)
+        models.storage.save()
+        models.storage.reload()
+        objs = FileStorage._FileStorage__objects
+        self.assertIn(f"BaseModel.{obj_base.id}", objs)
+        self.assertIn(f"User.{obj_User.id}", objs)
+        self.assertIn(f"State.{obj_state.id}", objs)
+        self.assertIn(f"Amenity.{obj_amenity.id}", objs)
+        self.assertIn(f"City.{obj_city.id}", objs)
+        self.assertIn(f"Review.{obj_review.id}", objs)
+        self.assertIn(f"Place.{obj_place.id}", objs)
+     
 
 
 
